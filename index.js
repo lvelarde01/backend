e623a749e7b19b675563f977248d363986d5b2bd
luis velarde <velardeluisangel@gmail.com>
2023-07-07 10:55:33 -0400

    [fix] Allow all access

--- index.js
+++ index.js
@@ -74,7 +74,9 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(bodyParser.json({limit: '250mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: "250mb", extended: true, parameterLimit:50000}));
 app.use(bodyParser.text({ limit: '200mb' }));
